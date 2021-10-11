d634020ca5ac0b831f9de8dee0ca24c5962c4bab
David Jonathan <david.jonn1@hotmail.com>
2021-10-11 12:25:34 -0300

    Adicionar arquivos de projeto.

---
+++ BlocoNotas/Notas.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlocoNotas
+{
+    public partial class Notas : Form
+    {
+        public Notas()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Rtf = String.Empty;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void newToolStripButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Rtf = String.Empty;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Abrir arquivo";
+            openFileDialog1.FileName = "";
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "Arquivos RTF| *.rtf";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader leitor = new System.IO.StreamReader(openFileDialog1.FileName);
+                richTextBox1.Rtf = leitor.ReadToEnd();
+                leitor.Close();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Salvar arquivo";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.InitialDirectory = "C:\\";
+            saveFileDialog1.Filter = "Arquivo | *.rtf";
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter gravar = new System.IO.StreamWriter(saveFileDialog1.FileName);
+                gravar.Write(richTextBox1.Rtf);
+                gravar.Close();
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Salvar arquivo";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.InitialDirectory = "C:\\";
+            saveFileDialog1.Filter = "Arquivo | *.rtf";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter gravar = new System.IO.StreamWriter(saveFileDialog1.FileName);
+                gravar.Write(richTextBox1.Rtf);
+                gravar.Close();
+            }
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectedText == "") {
+                MessageBox.Show("Precisa selecionar um texto");
+            }
+            else {
+                Clipboard.SetText(richTextBox1.SelectedText);
+                richTextBox1.SelectedText = "";
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectedText == "")
+            {
+                MessageBox.Show("Precisa copiar um texto");
+            }
+            else
+            {
+                Clipboard.SetText(richTextBox1.SelectedText);
+            }
+            }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectedText = Clipboard.GetText();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void customizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+            }
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                richTextBox1.SelectionColor = colorDialog1.Color;
+            }
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            openToolStripMenuItem.PerformClick();
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            saveAsToolStripMenuItem.PerformClick();
+        }
+
+        private void cutToolStripButton_Click(object sender, EventArgs e)
+        {
+            cutToolStripMenuItem.PerformClick();
+        }
+
+        private void copyToolStripButton_Click(object sender, EventArgs e)
+        {
+            copyToolStripMenuItem.PerformClick();
+        }
+
+        private void pasteToolStripButton_Click(object sender, EventArgs e)
+        {
+            pasteToolStripMenuItem.PerformClick();
+        }
+    }
+}
